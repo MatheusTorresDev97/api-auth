512e5790bdf0b86c33d4b54a9bd2a313d408ebe6
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-11-28 11:28:23 -0300

    multiplas roles

--- api/src/middlewares/verifyUserAuthorization.js
+++ api/src/middlewares/verifyUserAuthorization.js
@@ -1,10 +1,14 @@
 const AppError = require("../utils/AppError")
 
+// roleToVerify = admin ou customer ou sales
+
+// ['admin', 'customer', 'sale'].includes('sale')
+
 function verifyUserAuthorization(roleToVerify) {
     return (req, res, next) => {
         const { role } = req.user
 
-        if (role !== roleToVerify) {
+        if (roleToVerify.includes(role)) {
             throw new AppError("Unauthorized", 401)
         }
 
