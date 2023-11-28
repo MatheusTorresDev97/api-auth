f6c85a91afb33ed7e494218990e3764acd7e12df
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-11-28 11:55:24 -0300

    Redirecionando rotas de acordo com perfil

--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -1,16 +1,33 @@
 import { BrowserRouter } from 'react-router-dom';
-
+import { USER_ROLE } from '../utils/roles';
 import { useAuth } from "../hooks/auth";
 
-import { AppRoutes } from './app.routes';
 import { AuthRoutes } from './auth.routes';
+import { AdminRoutes } from './admin.routes';
+import { CustomerRoutes } from './customer.routes';
+import { SaleRoutes } from './sale.routes';
+
+
 
 export function Routes() {
   const { user } = useAuth();
 
+  function AccessRoutes() {
+    switch(user.role) {
+      case USER_ROLE.ADMIN: 
+        return <AdminRoutes />;
+      case USER_ROLE.CUSTOMER:
+        return <CustomerRoutes />;
+      case USER_ROLE.SALE:
+        return <SaleRoutes />;
+        default: 
+        return <CustomerRoutes />;
+    }
+  }
+
   return (
     <BrowserRouter>
-      {user ? <AppRoutes /> : <AuthRoutes />}
+      {user ? <AccessRoutes /> : <AuthRoutes />}
     </BrowserRouter>
   );
 }
